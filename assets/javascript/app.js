362bfb1b8fc973ea5e17c64665fbbaf40482d070
unknown <charles.bowden@gmail.com>
2017-10-24 16:55:03 -0400

    Minor Work

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Javascript Trivia Game
+// Week 5 UNC Coding Camp
+// By Charles Bowden
+
+// Variables
+var questionBank = {
+	questions: [''], 
+	topic: "",
+	correct: "",
+	incorrect: "",
+	choices: "",
+};
+
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
